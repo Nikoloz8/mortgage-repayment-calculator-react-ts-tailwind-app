b4f137a994012a7239337676e68640f42e46e066
Nikoloz Tevdoradze <nikoloztevdoradze08@gmail.com>
2025-05-15 14:43:10 +0400

    Commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,20 @@
+import type { Dispatch, SetStateAction } from "react";
+
 type HeaderProps = {
     textPreset2: string
     textPreset4: string
     clearInput: () => void
+    setCalculateDisplay: Dispatch<SetStateAction<boolean>>
 }
 
-export default function Header({ textPreset2, clearInput, textPreset4 }: HeaderProps) {
+export default function Header({ textPreset2, clearInput, textPreset4, setCalculateDisplay }: HeaderProps) {
     return (
         <div className='flex justify-between w-[100%] mb-[16px]!'>
             <h1 className={textPreset2}>Mortgage Calculator</h1>
-            <button onClick={clearInput} className={`${textPreset4} cursor-pointer underline decoration-[1px] underline-offset-2`}>Clear All</button>
-        </div>)
+            <button onClick={() => {
+                setCalculateDisplay(false)
+                clearInput
+            }
+            } className={`${textPreset4} cursor-pointer underline decoration-[1px] underline-offset-2`}>Clear All</button>
+        </div >)
 }
